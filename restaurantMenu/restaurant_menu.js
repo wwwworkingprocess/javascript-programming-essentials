96e11fa36d3c2472276c46deda6b5ae46cfa910d
Gergely Marton <wwworkingprocess@gmail.com>
2026-01-18 12:32:04 -0500

    added prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $22', 'Pasta - $16', 'Burger - $14', 'Salmon - $18'];
+const dessertMenu = ['Cake - $10', 'Ice Cream - $6', 'Pudding - $7', 'Fruit Salad - $8'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         
