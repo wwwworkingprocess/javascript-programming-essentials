db3c20695be656a01f1d48e17279058421d8b9c1
Gergely Marton <wwworkingprocess@gmail.com>
2026-01-18 12:47:38 -0500

    displaying total length

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -38,9 +38,12 @@ function startTest() {
         wpm = Math.round((typedWords / timeElapsed) * 60);
     }
 
+    var totalLength = userTypedText.length;
+
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + totalLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
